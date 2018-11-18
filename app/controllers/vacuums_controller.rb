bfa024001dd492fcf3343ee9936ce683020e3fc3
Baraa Shrem <baraa.shrem@gmail.com>
2018-11-18 19:27:21 +0200

    added index show new create to vacuum controller

--- app/controllers/vacuums_controller.rb
+++ app/controllers/vacuums_controller.rb
@@ -1,2 +1,25 @@
 class VacuumsController < ApplicationController
+  before_action :set_vacuum, only: [:show, :destroy, :edit]
+  def index
+    @vacuums = Vacuum.all
+  end
+
+  def show
+  end
+
+  def new
+    @vacuum = Vacuum.new
+  end
+
+  def create
+    @vacuum = Vacuum.new (vacuum_params)
+    @vacuum.save
+
+    if @vacuum.save
+      redirect_to @vacuum
+    else
+      render :new
+    end
+  end
+
 end
