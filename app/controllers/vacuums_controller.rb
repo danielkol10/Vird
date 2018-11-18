2a3073ef81749083f8f65bbc1edecde67474d983
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-11-18 19:23:06 +0200

    created vacuum-controller methods update edit destroy

--- app/controllers/vacuums_controller.rb
+++ app/controllers/vacuums_controller.rb
@@ -1,2 +1,25 @@
 class VacuumsController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    @vacuum = Vacuum.find(params[:id])
+    @vacuum.update(vacuum_params)
+    redirect_to vacuum_path
+  end
+  def destroy
+    @vacuum.destroy
+    redirect_to vacuums_path
+  end
+
+  private
+
+  def vacuum_params
+    params.require(:vacuum).permit(:model, :price, :address)
+  end
+
+  def set_vacuum
+    @vacuum = Vacuum.find(params[:id])
+  end
 end
