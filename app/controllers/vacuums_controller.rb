03cb338cee05a420ca79a9703a9c70e5eab50d22
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-11-19 11:30:43 +0200

    created vacuums controller, only index uncommented for now

--- app/controllers/vacuums_controller.rb
+++ app/controllers/vacuums_controller.rb
@@ -1,17 +1,42 @@
 class VacuumsController < ApplicationController
+  before_action :set_vacuum, only: [:show, :destroy, :edit]
 
-  def edit
+  def index
+    @vacuums = Vacuumm.all? { |e|  }
   end
 
-  def update
-    @vacuum = Vacuum.find(params[:id])
-    @vacuum.update(vacuum_params)
-    redirect_to vacuum_path
-  end
-  def destroy
-    @vacuum.destroy
-    redirect_to vacuums_path
-  end
+  # def show
+
+  # end
+
+  # def new
+  #   @vacuum = Vacuum.new
+  # end
+
+  # def create
+  #   @vacuum = Vacuum.new(vacuum_params)
+  #   @vacuum.save
+
+  #   if @vacuum.save
+  #     redirect_to @vacuum
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   @vacuum = Vacuum.find(params[:id])
+  #   @vacuum.update(vacuum_params)
+  #   redirect_to vacuum_path
+  # end
+
+  # def destroy
+  #   @vacuum.destroy
+  #   redirect_to vacuums_path
+  # end
 
   private
 
