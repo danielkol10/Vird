9f9337434efa20d501a88d15c9583e9c23a95690
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-11-19 11:44:47 +0200

    andres updated to master

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_18_135905) do
-=======
+
+
 ActiveRecord::Schema.define(version: 2018_11_18_172145) do
->>>>>>> master
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
