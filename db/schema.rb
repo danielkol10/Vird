b7b6fcf3392a6ce65dcf99d876760df450194638
Jack <jack.cohen1997@gmail.com>
2018-11-20 16:08:10 +0200

    Default available change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_064650) do
+ActiveRecord::Schema.define(version: 2018_11_20_140317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_064650) do
     t.string "model"
     t.integer "price"
     t.string "address"
-    t.boolean "available", default: false
+    t.boolean "available", default: true
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
